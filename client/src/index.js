d92e64de41aa93136682c58f56527e2e3d6838b4
akshaychavhan <akshay@rtledgers.com>
2023-09-28 13:49:14 +0530

    THEME SETUP COMPLETED

--- client/src/index.js
+++ client/src/index.js
@@ -17,13 +17,13 @@ import {
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from 'redux-persist/integration/react';
-import persistReducer from 'redux-persist/es/persistReducer';
+// import persistReducer from 'redux-persist/es/persistReducer';
 
 
 const persistConfig = { key: "root", storage, version: 1 };
-const persistReducer = persistReducer(persistConfig, authReducer);
+const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
-  reducer: persistReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => {
     getDefaultMiddleware({
       serializableCheck: {
