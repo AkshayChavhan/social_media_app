d72f14a32a551037aaf426daaea1be61a7641e85
akshaychavhan <akshay@rtledgers.com>
2023-09-28 12:58:05 +0530

    Routes Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./scenes/homePage/index";
+import LoginPage from "./scenes/loginPage/index";
+import ProfilePage from "./scenes/profilePage/index";
+
+
+
 function App() {
+
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
