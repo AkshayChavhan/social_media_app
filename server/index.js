ed2fd57c07386becd61d5060a5bc9ce85df606e7
akshaychavhan <akshay@rtledgers.com>
2023-09-28 10:52:09 +0530

    Backend-routes-controller completed

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ import { fileURLToPath} from 'url';
 import authRoutes from "./routes/auth.js"
 import { register } from './controllers/auth.js';
 import { verifyToken } from './middleware/auth.js';
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { createPost } from "./controllers/posts.js"
 
 // CONFIGURATION
 const __filename = fileURLToPath(import.meta.url);
@@ -42,10 +45,12 @@ const upload = multer({storage});
 
 // Routes with files
 app.post("auth/register" , upload.single("picture") , verifyToken , register);
-
+app.post("/posts", verifyToken, upload.single("picture") , createPost);
 
 // ROUTES
-app.use("/auth" , authRoutes)
+app.use("/auth" , authRoutes);
+app.use("/users" , userRoutes);
+app.use("/posts" , postRoutes);
 
 // MONGOOSE SETUP
 
